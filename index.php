096398d45bebbf12b33d4e63b081c32345a9384d
Steevens98 <67281150+Steevens98@users.noreply.github.com>
2026-07-27 01:43:26 +0000

    cambios en el archivo index.php

--- index.php
+++ index.php
@@ -5,9 +5,27 @@
     <title>Taller PHP - ESPOL</title>
     <link rel="stylesheet" href="estilos.css">
 </head>
+
 <body>
+<div class="contenedor">
     <h1>Bienvenido al Taller de PHP</h1>
-    <p><a href="formulario.php">Registrarse</a></p>
-    <p><a href="ingreso.php">Iniciar sesión</a></p>
+
+    <p>Seleccione una opción para continuar.</p>
+
+    <div class="botones">
+        <form action="formulario.php">
+        <input type="submit" value="Registrarse">
+        </form>
+        
+        <form action="ingreso.php">
+        <input type="submit" value="Iniciar sesión">
+        </form>
+    </div>
+
+<div class="ayuda">
+    Sistema de registro e inicio de sesión para la gestión de tareas.
+</div>
+
+</div>
 </body>
 </html>
